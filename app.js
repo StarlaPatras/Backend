cf06ec2d8b9d0233354287f896b661ebad611772
Starla Patras <starlapatras@gmail.com>
2023-10-19 19:32:07 +0500

    add port env variable

--- app.js
+++ app.js
@@ -50,12 +50,15 @@ app.use((error, req, res, next) => {
   });
 });
 
+const port = Number(process.env.PORT);
+console.log(port);
+
 mongoose
   .connect(
     `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.0tsdls5.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
   )
   .then(
-    app.listen(5000, () => {
+    app.listen(port, () => {
       console.log("DB Connection succesful");
     })
   )
